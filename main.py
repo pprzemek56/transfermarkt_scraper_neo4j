9ec5236d42a02ece3c8ebc9d749713d2b2c06698
pprzemek56 <przemkopis@wp.pl>
2023-05-26 01:49:07 +0200

    work in progress on method for fetching all clubs from top 5 leagues

--- main.py
+++ main.py
@@ -1,8 +1,30 @@
+import requests
+from bs4 import BeautifulSoup
+
 from database import conn
+
+headers = {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537',
+}
+
+leagues_links = [
+    'https://www.transfermarkt.com/serie-a/startseite/wettbewerb/IT1',
+    'https://www.transfermarkt.com/ligue-1/startseite/wettbewerb/FR1',
+    'https://www.transfermarkt.com/premier-league/startseite/wettbewerb/GB1',
+    'https://www.transfermarkt.com/bundesliga/startseite/wettbewerb/L1',
+    'https://www.transfermarkt.com/laliga/startseite/wettbewerb/ES1'
+]
 
 
 def main():
-    pass
+    fetch_all_clubs()
+
+
+def fetch_all_clubs():
+    for link in leagues_links:
+        response = requests.get(leagues_links[0], headers=headers)
+        soup = BeautifulSoup(response.content, 'html.parser')
+        print(soup.find(class_='hauptlink no-border-links'))
 
 
 if __name__ == '__main__':
